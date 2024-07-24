2319f4661b30c9cf1fba8196ea90abda5158a147
Christopher Forti <christopher.forti.79@gmail.com>
2024-07-23 20:02:31 -0400

    Changed app.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
 import "./index.css";
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <header className="text-black">Blog</header>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="w-full bg-slate-200 dark:bg-slate-900">
+        <h1 className="text-black">Blog</h1>
+        <div>
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+        </div>
       </div>
     </>
   );
 }
-
-export default App;
