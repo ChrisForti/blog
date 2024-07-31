018312d269a055dbc2e768a07bcbadf2f2865b2a
Christopher Forti <christopher.forti.79@gmail.com>
2024-07-30 20:04:49 -0400

    Got post preview working and my first post rendering.

--- src/components/Mainpage.tsx
+++ src/components/Mainpage.tsx
@@ -1,13 +1,13 @@
-// import { PostPreview } from "./PostPreview";
+import { PostPreview } from "./PostPreview";
 import { data } from "../content/content";
 import { Post } from "./Post";
 
 export function Mainpage() {
   return (
     <main className="container m-0 flex h-[calc(100vh_-_12.5rem)] flex-col overflow-scroll bg-white p-4">
-      {/* {data.map((post) => {
+      {data.map((post) => {
         return <PostPreview post={post} />;
-      })} */}
+      })}
       <Post post={data[0]} />
     </main>
   );
