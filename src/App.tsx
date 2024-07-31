6928457c7549e49553a44fab0b402e99439458b3
Christopher Forti <christopher.forti.79@gmail.com>
2024-07-30 20:16:54 -0400

    More unused code deleted.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
-import { useState } from "react";
 import "./index.css";
 import { Mainpage } from "./components/Mainpage";
 import { Header } from "./components/Header";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="text-black">
+      <div>
         <Header />
         <Mainpage />
-        <div>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-        </div>
       </div>
     </>
   );
