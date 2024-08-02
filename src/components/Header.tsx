d68fad1d22310c3d4acaf15caa5b4a09674baeb2
Christopher Forti <christopher.forti.79@gmail.com>
2024-08-01 20:44:00 -0400

    Cleaning up header and navigation files.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,6 @@
-import { useState } from "react";
 import { Navigation } from "./Navigation";
 
 export function Header() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const menuToggle = () => {
-    setIsOpen(!isOpen);
-  };
   return (
     <header className="p-6container relative isolate mx-auto flex flex-wrap items-center justify-between overflow-hidden bg-gray-900">
       <a href="/" className="flex items-center">
@@ -15,28 +9,6 @@ export function Header() {
         </span>
         <span className="m-6 text-4xl font-semibold dark:text-white">Blog</span>
       </a>
-      <button
-        onClick={menuToggle}
-        type="button"
-        className="ml-3 inline-flex rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-      >
-        <span className="sr-only">Open main menu</span>
-        {/* <!-- Hamburger icon --> */}
-        <svg
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16m-7 6h7"
-          />
-        </svg>
-      </button>
-
       <Navigation />
     </header>
   );
