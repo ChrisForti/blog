bc3a0cbd38aaf0e6f1fe5dc3a1ea329b4950fa38
Christopher Forti <christopher.forti.79@gmail.com>
2024-08-04 18:23:09 -0400

    Changes to state handler in header.tsx.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,23 @@
 import { Navigation } from "./Navigation";
 
-export function Header() {
+type HeaderProps = {
+  setHeaderNavigation: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export function Header({ setHeaderNavigation }: HeaderProps) {
   return (
     <header className="p-5container relative mx-auto flex flex-wrap items-center justify-between overflow-hidden bg-gray-900">
-      <a className="flex items-center">
+      <button
+        onClick={() => {
+          setHeaderNavigation(0);
+        }}
+        className="flex items-center"
+      >
         <span className="m-6 whitespace-nowrap text-4xl font-semibold dark:text-white">
           Code Vessel Blog
         </span>
-      </a>
-      <Navigation />
+      </button>
+      <Navigation setSelectedPost={setSelectedPost} />
     </header>
   );
 }
