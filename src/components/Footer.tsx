5c94fe71408bc63a9e55a784c18c1a0a4cb7f479
Christopher Forti <christopher.forti.79@gmail.com>
2024-08-18 17:32:11 -0400

    Linked my logo with my site via an anchor tag.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,12 +8,14 @@ export function Footer() {
         <div className="mx-auto max-w-screen-xl px-4 pt-8 sm:px-6 lg:px-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <div className="flex justify-center sm:justify-start">
-              <img
-                className="rounded-full"
-                src={Mylogo}
-                width="50"
-                height="50"
-              />
+              <a href="https://codevessel.org">
+                <img
+                  className="rounded-full"
+                  src={Mylogo}
+                  width="50"
+                  height="50"
+                />
+              </a>
             </div>
             <p className="mt-4 text-center text-sm text-teal-300 lg:mt-0 lg:text-right">
               T&C &nbsp; Career &nbsp; Privacy & Policy &nbsp; Developers
