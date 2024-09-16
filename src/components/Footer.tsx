f73988e83dd6742c35c57b5543e0df82f9e65615
Christopher Forti <christopher.forti.79@gmail.com>
2024-09-16 18:58:53 -0400

    Changed color of Footer to match header.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <div>
       <hr />
-      <footer className="overscroll-none bg-white pb-4">
+      <footer className="overscroll-none bg-blue-800 pb-4">
         <div className="mx-auto max-w-screen-xl px-4 pt-8 sm:px-6 lg:px-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <div className="flex justify-center sm:justify-start">
