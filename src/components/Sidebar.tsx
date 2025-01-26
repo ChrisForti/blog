a0b8a632d0326c654df19b942703a12145dd5475
Christopher Forti <christopher.forti.79@gmail.com>
2025-01-25 19:39:50 -0500

    Implemented Postdata in Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { data } from "../posts/posts";
+import { PostData, data } from "../posts/posts";
 import { v4 as uuidv4 } from "uuid";
 
 type SidebarProps = {
@@ -9,6 +9,7 @@ type SidebarProps = {
 export function Sidebar({ setSelectedPost }: SidebarProps) {
   const [isOpen, setIsOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
+  const [] = useState(data);
 
   function toggleSidebar() {
     setIsOpen(!isOpen);
@@ -18,16 +19,25 @@ export function Sidebar({ setSelectedPost }: SidebarProps) {
     setSelectedPost(selection);
   }
 
-  // const filterPostsBySearchTerm = (
-  //   data: PostData[],
-  //   searchTerm: string,
-  // ): PostData[] => {
-  //   // Ensure the search term is a string and not empty.
-  //   const lowercasedSearchTerm = searchTerm.toLowerCase();
-  //   return data.filter((post) =>
-  //     post.title.toLowerCase().includes(lowercasedSearchTerm),
-  //   );
-  // };
+  const filterPostsBySearchTerm = (
+    data: PostData[],
+    searchTerm: string,
+  ): PostData[] => {
+    if (!searchTerm || searchTerm.trim() === "") {
+      return data;
+    }
+    // Ensure the search term is a string and not empty.
+
+    const lowercasedSearchTerm = searchTerm.toLowerCase();
+    return data.filter(
+      (post) =>
+        post.title && post.title.toLowerCase().includes(lowercasedSearchTerm),
+    );
+  };
+
+  function applyFilter(): void {
+    throw new Error("Function not implemented.");
+  }
 
   return (
     <div className="bg-sky-900">
@@ -36,19 +46,6 @@ export function Sidebar({ setSelectedPost }: SidebarProps) {
         onClick={toggleSidebar}
       >
         Sidebar
-        {/* <svg
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16m-7 6h7"
-          />
-        </svg> */}
       </button>
       <aside
         className={
@@ -62,28 +59,36 @@ export function Sidebar({ setSelectedPost }: SidebarProps) {
             placeholder="Search by Tag, or Title"
             className="w-full rounded-md bg-gray-500 p-2 outline-none"
             value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
+            onChange={(event) => setSearchTerm(event.target.value)}
+          />{" "}
+          <button
+            onClick={applyFilter}
+            className="ml-2 rounded-md bg-teal-500 p-2 text-white"
+          >
+            Filter
+          </button>
         </div>
         <div className="rounded-md bg-white">
           <h1 className="my-4 cursor-pointer rounded-md border-b-2 bg-teal-500 py-2 text-center text-xl text-gray-600">
             Posts
           </h1>
           <div className="list-none rounded-md bg-white text-center">
-            {data.map((_, index) => {
-              const postNumber = index + 1;
-              return (
-                <li
-                  key={uuidv4()}
-                  className="text-1xl border-b-2 bg-clip-text py-3 font-bold text-teal-500 hover:cursor-pointer"
-                  onClick={() => {
-                    selectPost(postNumber);
-                  }}
-                >
-                  Post {postNumber}
-                </li>
-              );
-            })}
+            {filterPostsBySearchTerm(data, searchTerm).map(
+              ({ title }, index) => {
+                const postNumber = index + 1;
+                return (
+                  <li
+                    key={uuidv4()}
+                    className="text-1xl border-b-2 bg-clip-text py-3 font-bold text-teal-500 hover:cursor-pointer"
+                    onClick={() => {
+                      selectPost(postNumber);
+                    }}
+                  >
+                    Post {postNumber}:{title}
+                  </li>
+                );
+              },
+            )}
           </div>
         </div>
       </aside>
